c576337346b45d70b397e3ec013e8735a8aeb116
MahadMohamoud941 <mahadmohamoud1997@outlook.com>
2025-01-18 16:20:20 +0000

    added arithmatic functions

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello world")
+# # def greet (firstname "not applicable", lastname , age "na", city "unknown")
+# #     print("welcome to skills city" , firstname, lastname, age, city)    
+# # print ( "your name is, age is , city is")
+
+# # arithmetic functions 
+
+# # def sum (a,b):
+# #     return a + b
+# # def sub (a,b):
+# #     return a - b
+# # def mul (a,b):
+# #     return a * b
+# # def div (a,b):
+# #     return a / b
+
+# # print(sum (2,3)) # 5
+# # print(sub (2,3)) # -1
+# # print(mul (2,3)) # 6
+# # print(div (2,3)) # 0.666666666666666
+
+# # what is the difference between function and procedure.
+# # function returns a value, procedure does not return a value
+
+# name = "mahad" 
+# def my_function():
+#     print ("hello", name)
+# my_function()
+
+# # name = "mahad" # global variable accessable to all functions
+# # def my_function():
+# #     name = "ali" # local variable only accessable to this function
+# #     print ("hello", name)
+
+
---
+++ mymodule.py
@@ -0,0 +1,13 @@
+# # def sum (a,b):
+# #     return a + b
+# # def sub (a,b):
+# #     return a - b
+# # def mul (a,b):
+# #     return a * b
+# # def div (a,b):
+# #     return a / b
+
+# # print(sum (2,3)) # 5
+# # print(sub (2,3)) # -1
+# # print(mul (2,3)) # 6
+# # print(div (2,3)) # 0.666666666666666
